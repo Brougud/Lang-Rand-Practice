072f6720e644a8413e254edf3dafa77f0c14dd04
Monke <85203534+Brougud@users.noreply.github.com>
2021-08-24 11:30:11 -0400

    Create Prims.c

---
+++ C/Prims.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int integer = 5; //int is whole number
+  float floatvar = 3.14; //float a var ex. 19.2934283628
+  char string[] = "Hello, World!"; //string is a text
+
+  printf("%d is an integer!\n", integer);
+  printf("%f is a float!\n", floatvar);
+  printf("%s is a char!\n", string);
+
+}
