9db74df2511a5f3186bcd3ea3927def150445848
Monke <85203534+Brougud@users.noreply.github.com>
2021-08-24 11:33:53 -0400

    Create UserPass.c

---
+++ C/UserPass.c
@@ -0,0 +1,14 @@
+#import <stdio.h>
+
+int main(void) {
+  
+  char firstN[20];
+  char lastN[20];
+  
+  printf("Enter your first name: ");
+  scanf("%s", &first);
+  printf("Enter your last name: ");
+  scanf("%s", &last);
+  
+  printf("Hello %s %s !\n", firstN, lastN)
+}
